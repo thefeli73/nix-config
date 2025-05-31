c9850013fe407de1e23320994279dd076cdcbc04
Felix Schulze <admin@schulze.network>
2025-05-31 19:06:15 +0200

    increase generation backup age
    it doesnt take that much space lol

--- rebuild-nix-system.sh
+++ rebuild-nix-system.sh
@@ -6,7 +6,7 @@ nix flake check
 # Rebuild the system
 sudo nixos-rebuild switch --flake ./#default
 
-# Clean up old generations older than 90 days
-sudo nix-collect-garbage --delete-older-than 90d 
+# Clean up old generations older than 180 days
+sudo nix-collect-garbage --delete-older-than 180d 
 
 
