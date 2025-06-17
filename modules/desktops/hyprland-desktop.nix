77a9f3f30f1fdd1625ca33d4357e8a6271eb37bb
Felix Schulze <admin@schulze.network>
2025-06-17 16:47:11 +0200

    hurricane: 21 current  2025-06-17 16:47:10  25.05.20250615.6c64dab     6.15.2                          *
    clean up

--- modules/desktops/hyprland-desktop.nix
+++ modules/desktops/hyprland-desktop.nix
@@ -66,20 +66,16 @@
 
   environment.sessionVariables = {
     BROWSER = "${pkgs.lib.getBin pkgs.firefox}";
-    GTK_THEME = "Gruvbox-Dark-B"; # or whatever your installed variant is called
-    QT_QPA_PLATFORMTHEME = "qt5ct";
     NIXOS_OZONE_WL = "1";
-    # XCURSOR_THEME = "Gruvbox-Dark";    # (Optional)
   };
 
   environment.systemPackages = with pkgs; [
     # Core Hyprland workflow tools
     waybar # Panel
-    nwg-look # GTK theme tweaker
-    rofi-wayland # Launcher (or fuzzel/wofi/tofi)
+    rofi-wayland # Launcher
     mako # Notification daemon
-    hyprpaper # Wallpaper daemon (native)
-    hyprlock # Lock screen (native)
+    hyprpaper # Wallpaper daemon
+    hyprlock # Lock screen
     wl-clipboard # Clipboard utils
     cliphist # Clipboard manager
     pavucontrol # GUI audio mixer
@@ -93,10 +89,6 @@
     wf-recorder # Screenshots & screenrecording
     libsForQt5.qt5ct # For QT application appearance
     nautilus # File manager
-    # Install the Gruvbox GTK theme (many are packaged, else use overlays)
-    gruvbox-gtk-theme
-    # Gruvbox icons/cursors if desired
-    gruvbox-dark-icons-gtk
   ];
 
   programs.nautilus-open-any-terminal = {
