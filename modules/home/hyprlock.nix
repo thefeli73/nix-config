c85c696508e3f0c8bc93061bf9f314039c9a5052
Felix Schulze <admin@schulze.network>
2025-06-18 15:50:05 +0200

    wildfire: 190 current  2025-06-18 15:50:01  25.05.20250615.6c64dab  6.15.2                          *
    hyprlock

--- modules/home/hyprlock.nix
+++ modules/home/hyprlock.nix
@@ -5,7 +5,7 @@ in {
     enable = true;
     settings = {
       background = {
-        path = "$HOME/git/nixos/modules/home/images/sunset-rocks.png";
+        path = "$HOME/git/nixos/modules/home/images/sky.png";
         blur_passes = 2;
         contrast = 1;
         brightness = 0.5;
@@ -30,13 +30,12 @@ in {
         dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
         dots_spacing = 0.35; # Scale of dots' absolute size, 0.0 - 1.0
         dots_center = true;
-        outer_color = "rgba(0, 0, 0, 0)";
-        inner_color = "rgba(0, 0, 0, 0.2)";
+        outer_color = colors.gruvbox.blue;
+        inner_color = colors.gruvbox.bg0;
         font_color = colors.gruvbox.fg1;
         fade_on_empty = false;
         rounding = -1;
         check_color = colors.gruvbox.yellow;
-        placeholder_text = "<i><span foreground=\"${colors.gruvbox.fg3}\">Input Password...</span></i>";
         hide_input = false;
         position = "0, -200";
         halign = "center";
@@ -47,9 +46,10 @@ in {
       label = [
         {
           monitor = "";
-          text = "cmd[update:10000] $(date +\"%A, %B %d\")";
+          text = "cmd[update:10000] echo \"$(date +\"%A, %B %d\")\"";
           color = colors.gruvbox.fg2;
           font_size = 34;
+          font_family = "Intel One Mono";
           position = "0, 300";
           halign = "center";
           valign = "center";
@@ -58,9 +58,10 @@ in {
         # TIME
         {
           monitor = "";
-          text = "cmd[update:2000] $(date +\"%-I:%M\")";
+          text = "cmd[update:2000] echo \"$(date +\"%H:%M\")\"";
           color = colors.gruvbox.fg1;
           font_size = 94;
+          font_family = "Intel One Mono Bold";
           position = "0, 200";
           halign = "center";
           valign = "center";
