73ca522ca2c64f34419ad71f8fb1436cf3a34466
Felix Schulze <admin@schulze.network>
2025-06-18 13:12:00 +0200

    wildfire: 180 current  2025-06-18 13:11:57  25.05.20250615.6c64dab  6.15.2                          *
    hyprland config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -4,6 +4,15 @@
   wayland.windowManager.hyprland.settings = {
     input = {
       kb_layout = "se";
+      kb_variant = "";
+      kb_model = "";
+      kb_options = "";
+      kb_rules = "";
+      kb_file = "";
+
+      follow_mouse = 1;
+
+      sensitivity = 0;
     };
     "$mod" = "SUPER";
     "$terminal" = "ghostty";
@@ -14,18 +23,39 @@
       gaps_in = 4;
       gaps_out = 4;
       border_size = 1;
-      "col.active_border" = "rgba(ebdbb2ee) rgba(fe8019ee) 45deg";
+      "col.active_border" = "rgba(fe8019ff) rgba(cc241dff) 45deg";
       "col.inactive_border" = "rgba(7c6f64aa)";
+      layout = "dwindle";
+      allow_tearing = false;
     };
     decoration = {
-      rounding = "8";
-      rounding_power = "3";
+      rounding = 8;
+      rounding_power = 3.5;
       blur = {
-        enabled = "true";
-        xray = "true";
-        size = "3";
-        passes = "4";
+        enabled = true;
+        xray = true;
+        size = 3;
+        passes = 4;
       };
+    };
+
+    animations = {
+      enabled = true;
+      bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+      animation = [
+        "windows, 1, 7, myBezier"
+        "windowsOut, 1, 7, default, popin 80%"
+        "border, 1, 10, default"
+        "borderangle, 1, 8, default"
+        "fade, 1, 7, default"
+        "workspaces, 1, 6, default"
+      ];
+    };
+
+    dwindle = {
+      pseudotile = true;
+      preserve_split = true;
     };
 
     # Bindings
@@ -36,6 +66,14 @@
         "$mod, E, exec, $filemanager"
         "$mod, SPACE, exec, $menu"
         ", Print, exec, grimblast copy area"
+        "$mod, V, togglefloating"
+        "$mod, J, togglesplit, " # dwindle
+
+        # Move focus with mainMod + arrow keys
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
       ]
       ++ (
         # workspaces
@@ -50,5 +88,10 @@
           )
           9)
       );
+    # Move/resize windows with mainMod + LMB/RMB and dragging
+    bindm = [
+      "$mod, mouse:272, movewindow"
+      "$mod, mouse:273, resizewindow"
+    ];
   };
 }
