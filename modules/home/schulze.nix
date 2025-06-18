67ada5577dcab54b1e478081b7d1b0bfabea5a04
Felix Schulze <admin@schulze.network>
2025-06-18 08:46:31 +0200

    wildfire: 168 current  2025-06-18 08:46:28  25.05.20250615.6c64dab  6.15.2                          *

--- modules/home/schulze.nix
+++ modules/home/schulze.nix
@@ -58,7 +58,7 @@
       );
   };
   wayland.windowManager.hyprland.extraConfig = ''
-    monitor = DP-3, 2560x1440@143.97, 1920x0, auto;
+    monitor = DP-3, 2560x1440@143.97, 1920x-360, auto;
     monitor = HDMI-A-1, 1920x1080@60, 0x0, auto;
   '';
 }
