46b4ba80a94be61f4b5bc22d43fe2a58ebf94ab6
Felix Schulze <admin@schulze.network>
2025-06-20 16:28:13 +0200

    hyprland monitors refactor config

--- hosts/hurricane/hyprland-monitors.nix
+++ hosts/hurricane/hyprland-monitors.nix
@@ -1,8 +1,8 @@
 {
   # Home Manager Hyprland monitors
-  wayland.windowManager.hyprland.extraConfig = ''
-    monitor = DP-3, 3440x1440@99.98, 0x0, auto; # Philips Ultrawide left
-    monitor = HDMI-A-1, 3440x1440@99.98, 3440x0, auto; # Philips Ultrawide right
-    monitor = eDP-1, 1920x1200@60.00, auto, auto # internal display
-  '';
+  wayland.windowManager.hyprland.settings.monitor = [
+    "DP-3, 3440x1440@99.98, 0x0, 1" # Philips Ultrawide left
+    "HDMI-A-1, 3440x1440@99.98, 3440x0, 1" # Philips Ultrawide right
+    "eDP-1, 1920x1200@60.00, 1" # internal display
+  ];
 }
