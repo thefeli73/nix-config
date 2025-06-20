0b78f3aa91d99efbedb77a09f4dab25389f78802
Felix Schulze <admin@schulze.network>
2025-06-20 10:07:36 +0200

    wildfire: 229 current  2025-06-20 10:07:32  25.05.20250617.36ab78d  6.15.2                          *

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -1,7 +1,6 @@
 {config, ...}: {
   programs.rofi = {
     enable = true;
-    font = "Intel One Mono 14";
     modes = [
       "drun"
       "window"
@@ -11,60 +10,76 @@
       inherit (config.lib.formats.rasi) mkLiteral;
     in {
       "*" = {
+        font = "Intel One Mono 14";
         border = 0;
         margin = 0;
         padding = 0;
         spacing = 0;
 
-        bg = mkLiteral "transparent";
-        bg-alt = mkLiteral "#3c3836";
-        fg = mkLiteral "#fbf1c7";
+        bg0 = mkLiteral "#282828f2";
+        bg1 = mkLiteral "#3c3836";
+        bg2 = mkLiteral "#50494580";
+        bg3 = mkLiteral "#665c54f2";
+
+        fg0 = mkLiteral "#fbf1c7";
+        fg1 = mkLiteral "#ebdbb2";
+        fg2 = mkLiteral "#d5c4a1";
+        fg3 = mkLiteral "#bdae93";
+
         blue = mkLiteral "#83a598";
         orange = mkLiteral "#d65d0e";
         blue-alt = mkLiteral "#458588";
         orange-alt = mkLiteral "#fe8019";
 
-        background-color = mkLiteral "@bg";
-        text-color = mkLiteral "@fg";
+        background-color = mkLiteral "@bg0";
+        text-color = mkLiteral "@fg0";
       };
 
       "window" = {
-        transparency = mkLiteral "'real'";
+        background-color = mkLiteral "@bg0";
+        #transparency = mkLiteral "'real'";
       };
 
       "mainbox" = {
-        children = mkLiteral "[inputbar, listview]";
+        padding = mkLiteral "12px";
+        #children = mkLiteral "[inputbar, listview]";
       };
 
       "inputbar" = {
-        background-color = mkLiteral "@bg-alt";
-        children = mkLiteral "[prompt, entry]";
-        border-color = mkLiteral "@blue";
-      };
+        background-color = mkLiteral "@bg1";
+        border-color = mkLiteral "@bg3";
 
-      "entry" = {
-        background-color = mkLiteral "@blue-alt";
-        text-color = mkLiteral "@blue";
-        padding = mkLiteral "12px 3px";
+        border = mkLiteral "2px";
+
+        padding = mkLiteral "8px 16px";
+        spacing = mkLiteral "8px";
+        children = mkLiteral "[prompt, entry]";
       };
 
       "prompt" = {
-        background-color = mkLiteral "inherit";
-        padding = mkLiteral "12px 16px 12px 12px";
+        text-color = mkLiteral "@fg2";
+      };
+
+      "entry" = {
+        placeholder = "Search";
+        placeholder-color = mkLiteral "@fg3";
       };
 
       "listview" = {
+        background-color = mkLiteral "transparent";
+        margin = mkLiteral "12px 0 0";
+        columns = 1;
         lines = 10;
       };
 
       "element" = {
         border = mkLiteral "0 0 0 2px";
-        children = mkLiteral "[element-text]";
         padding = mkLiteral "8px 16px";
         spacing = mkLiteral "8px";
       };
 
       "element selected" = {
+        text-color = mkLiteral "@bg1";
         border-color = mkLiteral "@orange";
       };
 
