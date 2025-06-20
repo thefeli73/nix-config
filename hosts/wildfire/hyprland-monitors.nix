472977c77183050c99ca5c759a0c41c2ed4fa5c2
Felix Schulze <admin@schulze.network>
2025-06-20 14:25:29 +0200

    wildfire: 254 current  2025-06-20 14:25:24  25.05.20250617.36ab78d  6.15.2                          *
    wildfire: 255 current  2025-06-20 14:26:40  25.05.20250617.36ab78d  6.15.2                          *
    
    wildfire: 256 current  2025-06-20 14:28:25  25.05.20250617.36ab78d  6.15.2                          *

--- hosts/wildfire/hyprland-monitors.nix
+++ hosts/wildfire/hyprland-monitors.nix
@@ -1,7 +1,7 @@
 {
   # Home Manager Hyprland monitors
   wayland.windowManager.hyprland.extraConfig = ''
-    monitor = DP-3, 2560x1440@143.97, 1920x-360, auto;
-    monitor = HDMI-A-1, 1920x1080@59.94, 0x0, auto;
+    monitor = DP-3, 2560x1440@143.97, 1920x-360, 1, vrr, 1;
+    monitor = HDMI-A-1, 1920x1080@59.94, 0x0, 1;
   '';
 }
