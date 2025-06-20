7533a9a2f3207fc8502b74c5c47d1ba248e73cfd
Felix Schulze <admin@schulze.network>
2025-06-20 09:55:17 +0200

    fix monitor wildfire

--- hosts/wildfire/hyprland-monitors.nix
+++ hosts/wildfire/hyprland-monitors.nix
@@ -2,6 +2,6 @@
   # Home Manager Hyprland monitors
   wayland.windowManager.hyprland.extraConfig = ''
     monitor = DP-3, 2560x1440@143.97, 1920x-360, auto;
-    monitor = HDMI-A-1, 1920x1080@60, 0x0, auto;
+    monitor = HDMI-A-1, 1920x1080@59.94, 0x0, auto;
   '';
 }
