8ffe52a3cad25215215d10e31d7cf072f8632f97
Felix Schulze <admin@schulze.network>
2025-06-21 13:01:34 +0200

    nextcloud fix?
    issue client starting twice...

--- modules/home/nextcloud.nix
+++ modules/home/nextcloud.nix
@@ -1,11 +1,6 @@
-{lib, ...}: {
+{
   services.nextcloud-client = {
     enable = true;
     startInBackground = true;
-  };
-  systemd.user.services.nextcloud-client = {
-    Unit = {
-      After = lib.mkForce "graphical-session.target";
-    };
   };
 }
