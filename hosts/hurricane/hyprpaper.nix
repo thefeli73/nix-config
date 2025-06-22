c233099b99e8310e75ad875828bb9486142b7e5e
Felix Schulze <admin@schulze.network>
2025-06-22 14:27:35 +0200

    backgrounds

--- hosts/hurricane/hyprpaper.nix
+++ hosts/hurricane/hyprpaper.nix
@@ -9,6 +9,8 @@
       wallpaper = [
         ", $HOME/git/nix-config/modules/home/images/nix.png"
         "DP-3, $HOME/git/nix-config/modules/home/images/forest.png"
+        "HDMI-A-1, $HOME/git/nix-config/modules/home/images/forest.png"
+        "eDP-1, $HOME/git/nix-config/modules/home/images/forest.png"
       ];
     };
   };
