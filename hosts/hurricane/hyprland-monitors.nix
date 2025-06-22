6d61866827b8565e47d68b5c53edce4dbe4d7558
Felix Schulze <admin@schulze.network>
2025-06-22 13:11:48 +0200

    hurricane: 44 current  2025-06-22 13:11:44  25.05.20250617.36ab78d     6.15.2                          *

--- hosts/hurricane/hyprland-monitors.nix
+++ hosts/hurricane/hyprland-monitors.nix
@@ -3,6 +3,6 @@
   wayland.windowManager.hyprland.settings.monitor = [
     "DP-3, 3440x1440@99.98, 0x0, 1" # Philips Ultrawide left
     "HDMI-A-1, 3440x1440@99.98, 3440x0, 1" # Philips Ultrawide right
-    "eDP-1, 1920x1200@60.00, 1" # internal display
+    "eDP-1, 1920x1200@60.00, auto, 1" # internal display
   ];
 }
