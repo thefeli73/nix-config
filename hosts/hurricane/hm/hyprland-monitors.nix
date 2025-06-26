9432b4d51e80dcd290e01817b7e343bf34151b7f
Felix Schulze <admin@schulze.network>
2025-06-26 15:38:23 +0200

    hurricane: 74 current  2025-06-26 15:38:16  25.05.20250620.b2485d5     6.15.3                          *
    laptop lid close turn off screen

--- hosts/hurricane/hm/hyprland-monitors.nix
+++ hosts/hurricane/hm/hyprland-monitors.nix
@@ -1,9 +1,15 @@
 {
   # Home Manager Hyprland monitors
-  wayland.windowManager.hyprland.settings.monitor = [
-    "DP-3, 3440x1440@99.98, 0x0, 1" # Philips Ultrawide left
-    "HDMI-A-1, 3440x1440@99.98, 3440x0, 1" # Philips Ultrawide right
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "DP-3, 3440x1440@99.98, 0x0, 1" # Philips Ultrawide left
+      "HDMI-A-1, 3440x1440@99.98, 3440x0, 1" # Philips Ultrawide right
 
-    "eDP-1, 1920x1200@60.00, 6880x240, 1" # internal display
-  ];
+      "eDP-1, 1920x1200@60.00, 6880x240, 1" # internal display
+    ];
+    bindl = [
+      ",switch:off:Lid Switch,exec,hyprctl keyword monitor 'eDP-1, 1920x1200@60.00, 6880x240, 1'" # enable display in hyprland when lid open
+      ",switch:on:Lid Switch,exec,hyprctl keyword monitor 'eDP-1, disable'" # disable display in hyprland when lid closed
+    ];
+  };
 }
