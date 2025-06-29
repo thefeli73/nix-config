3ed9534d00be8021bf164f462d22da42a719584c
Felix Schulze <admin@schulze.network>
2025-06-29 21:16:51 +0200

    hypridle

--- modules/hm/hypridle.nix
+++ modules/hm/hypridle.nix
@@ -11,23 +11,19 @@
 
       listener = [
         {
-          timeout = 180; # 3min.
+          timeout = 300; # 5min.
           on-timeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
           on-resume = "brightnessctl -r"; # monitor backlight restore.
         }
         {
-          timeout = 600; # 10min.
+          timeout = 900; # 15min.
           on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
         }
         {
-          timeout = 900; # 15min.
+          timeout = 1800; # 30min.
           on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
           on-resume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
         }
-        #{
-        #  timeout = 1800; # 30min.
-        #  on-timeout = "systemctl suspend"; # suspend pc
-        #}
       ];
     };
   };
