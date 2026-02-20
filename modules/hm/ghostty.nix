a9cf39bc538650caf34e1b0aeeb4a3b71f8840d2
Felix Schulze <felix.schulze@brainmill.com>
2026-02-20 16:18:28 +0100

    wildfire: 525 - 6.19.2 (25.11.20260218.6d41bc2)
    ghostty font-size

--- modules/hm/ghostty.nix
+++ modules/hm/ghostty.nix
@@ -4,6 +4,7 @@
     enableFishIntegration = true;
     settings = {
       "font-family" = "Intel One Mono";
+      "font-size" = 10;
       "theme" = "Gruvbox Dark";
       "background-opacity" = 0.6;
       "mouse-scroll-multiplier" = 1.0;
