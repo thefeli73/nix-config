d6c2d5ad3408ef63197466cdafba563e8460a22d
Felix Schulze <felix.schulze@brainmill.com>
2026-03-11 14:58:38 +0100

    wildfire: 538 - 6.19.6 (25.11.20260306.71caefc)
    docker bake/buildx fix

--- modules/hm/home-manager.nix
+++ modules/hm/home-manager.nix
@@ -1,3 +1,6 @@
-{
-  home.stateVersion = "25.05"; # Dont change
+{pkgs, ...}: {
+  home = {
+    stateVersion = "25.05"; # Dont change
+    file.".docker/cli-plugins/docker-buildx".source = "${pkgs.docker-buildx}/libexec/docker/cli-plugins/docker-buildx";
+  };
 }
