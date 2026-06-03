95d063818e1f4ab45b5f1f05770d44974bd6e6a7
Felix Schulze <admin@schulze.network>
2026-06-03 14:35:28 +0200

    disable remmina autostart

--- modules/hm/home-manager.nix
+++ modules/hm/home-manager.nix
@@ -3,4 +3,16 @@
     stateVersion = "25.05"; # Dont change
     file.".docker/cli-plugins/docker-buildx".source = "${pkgs.docker-buildx}/libexec/docker/cli-plugins/docker-buildx";
   };
+
+  xdg.configFile."autostart/remmina-applet.desktop".text = ''
+    [Desktop Entry]
+    Version=1.0
+    Name=Remmina Applet
+    Comment=Connect to remote desktops through the applet menu
+    Icon=org.remmina.Remmina
+    Exec=remmina -i
+    Terminal=false
+    Type=Application
+    Hidden=true
+  '';
 }
