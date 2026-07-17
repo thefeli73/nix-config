32e9e7a960d33fd5bd785296aacd3482a388132b
Felix Schulze <git@schulze.network>
2026-07-17 10:59:57 +0200

    oc: update omo slim

--- modules/hm/opencode.nix
+++ modules/hm/opencode.nix
@@ -1,5 +1,5 @@
 {pkgs-unstable, ...}: let
-  ohMyOpenCodeSlimPlugin = "oh-my-opencode-slim@2.1.1";
+  ohMyOpenCodeSlimPlugin = "oh-my-opencode-slim@2.2.3";
   dcpPlugin = "@tarquinen/opencode-dcp@3.1.14";
 in {
   programs.opencode = {
