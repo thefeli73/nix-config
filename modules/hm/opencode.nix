e522e5dc7a1b33b4079ec11e530cbe150fca0101
Felix Schulze <admin@schulze.network>
2026-09-08 10:10:42 +0200

    Revert "oc: support gpt-6"
    This reverts commit fe3a4d61befb281e84d696450c54e89583e37110.

--- modules/hm/opencode.nix
+++ modules/hm/opencode.nix
@@ -4,20 +4,7 @@
 in {
   programs.opencode = {
     enable = true;
-    package = pkgs-unstable.opencode.overrideAttrs (old: {
-      patches =
-        (old.patches or [])
-        ++ [
-          (pkgs-unstable.fetchurl {
-            url = "https://github.com/anomalyco/opencode/commit/500c46ec79b818a44f7962e0707415ac3227c753.patch";
-            hash = "sha256-e+aRnmnmO4kRgIdWjqeL4ttLzcpXKU3/giW9xY3V9Q0=";
-          })
-          (pkgs-unstable.fetchurl {
-            url = "https://github.com/anomalyco/opencode/commit/02a167e048d3bd7299225068d79e4fce5c830d67.patch";
-            hash = "sha256-ADDTVlz3V5/GzkmLaMTIg5NDDF7Z8B6LJOTLAs/jhro=";
-          })
-        ];
-    });
+    package = pkgs-unstable.opencode;
     commands.guardrail = ./opencode/commands/guardrail.md;
     settings = {
       autoupdate = false;
